0ee2ae4f594d0cd6b35e7517b28cae8d70dca5fe
Antonin GAVREL <angavrel@e2r10p17.42.fr>
2017-06-14 21:57:16 +0200

    [Add] Routing (video #6 from youtube tutorials

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 /* http://localhost:8000/ */
 
 var http = require('http');
+var app = require('./app');
+
+http.createServer(app.handleRequest).listen(8000);
+
+/*
 var fs = require('fs');
 var module1 = require('./module1');
 var module2 = require('./module2');
@@ -13,7 +18,7 @@ function onRequest(request, response)
 	response.write(module1.myString);
 	response.write(module2.myVariable);
 	module1.myFunction();
-	module2.myFunction();*/
+	module2.myFunction();
 	fs.readFile('./index.html', null, function(error, data) {
 		if (error) {
 			response.writeHead(404);
@@ -25,4 +30,4 @@ function onRequest(request, response)
 	});
 }
 
-http.createServer(onRequest).listen(8000);
+http.createServer(onRequest).listen(8000);*/
