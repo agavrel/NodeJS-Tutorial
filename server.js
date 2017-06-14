ce3e0dbb2ace0ebfe205d4ad702471233a8f5a80
Antonin GAVREL <angavrel@e2r10p17.42.fr>
2017-06-14 21:42:15 +0200

    [INIT]Node JS tutorial

---
+++ server.js
@@ -0,0 +1,28 @@
+/* http://localhost:8000/ */
+
+var http = require('http');
+var fs = require('fs');
+var module1 = require('./module1');
+var module2 = require('./module2');
+
+function onRequest(request, response)
+{
+//	response.writeHead(200, {'Content-Type': 'text/plain'});
+	response.writeHead(200, {'Content-Type': 'text/html'});
+/*	response.write('Hello World');
+	response.write(module1.myString);
+	response.write(module2.myVariable);
+	module1.myFunction();
+	module2.myFunction();*/
+	fs.readFile('./index.html', null, function(error, data) {
+		if (error) {
+			response.writeHead(404);
+			response.write('File not found !');
+		} else {
+			response.write(data);
+		}
+		response.end();
+	});
+}
+
+http.createServer(onRequest).listen(8000);
